6d3eab4181e7d93dcd533c527acbba5f45e8a82d
QiquanZhang <949245661@qq.com>
2026-09-22 16:49:28 +0800

    zh overview tagline line2: 落地有声 (drop 有据)

--- i18n.js
+++ i18n.js
@@ -138,7 +138,7 @@
   };
 
   const OVERVIEW_EN = 'assets/overview.svg?v=25';
-  const OVERVIEW_ZH = 'assets/overview-zh.svg?v=4';
+  const OVERVIEW_ZH = 'assets/overview-zh.svg?v=5';
   function applyOverview() {
     const figure = document.querySelector('#overview');
     if (!figure) return;
