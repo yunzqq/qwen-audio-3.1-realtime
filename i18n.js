1b0fd16101a1f3b009a1625dbc2c6bab116a29e0
QiquanZhang <949245661@qq.com>
2026-09-22 17:08:39 +0800

    zh overview: enlarge core verbs to 26px

--- i18n.js
+++ i18n.js
@@ -138,7 +138,7 @@
   };
 
   const OVERVIEW_EN = 'assets/overview.svg?v=25';
-  const OVERVIEW_ZH = 'assets/overview-zh.svg?v=10';
+  const OVERVIEW_ZH = 'assets/overview-zh.svg?v=11';
   function applyOverview() {
     const figure = document.querySelector('#overview');
     if (!figure) return;
